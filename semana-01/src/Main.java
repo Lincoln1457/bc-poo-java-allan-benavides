c09cc6e9159fc4c265125f545c08651bb0edced4
Koll <136403733+Lincoln1457@users.noreply.github.com>
2025-11-30 12:00:53 -0500

    modificación

--- semana-01/src/Main.java
+++ semana-01/src/Main.java
@@ -1,28 +1,25 @@
 public class Main {
   public static void main(String[] args) {
 
-    // Crear un objeto Miembro
     Miembro miembro1 = new Miembro(
         "Anderson Chala",
         25,
         72.5,
+        "anderlindo@gmail.com",
         false,
         0
     );
 
-    // Imprimir información del miembro
     miembro1.imprimirInfo();
 
-    // Calcular IMC
-    double imc = miembro1.calcularIMC(1.75); // altura en metros
-    System.out.println("IMC calculado: " + imc);
+    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-    // Usar getter
+    double imc = miembro1.calcularIMC(1.75);
+    System.out.println("IMC del que se hizo cálclo: " + imc);
+
     System.out.println("Nombre del miembro: " + miembro1.getNombreCompleto());
 
-    // Usar setter
     miembro1.setPeso(78.0);
     System.out.println("Nuevo peso: " + miembro1.calcularIMC(1.75));
   }
 }
-
--- semana-01/src/Miembro.java
+++ semana-01/src/Miembro.java
@@ -1,42 +1,38 @@
 public class Miembro {
+  private String nombreCompleto;
+  private int edad;
+  private double peso;
+  private String email;
+  private boolean membresiaActiva;
+  private int sesionesRestantes;
 
-  // Atributos
-  private String nombreCompleto;   // String
-  private int edad;                // int
-  private double peso;             // double
-  private boolean membresiaActiva; // boolean
-  private int sesionesRestantes;   // int
-
-  // Constructor
-  public Miembro(String nombreCompleto, int edad, double peso, boolean membresiaActiva, int sesionesRestantes) {
+  public Miembro(String nombreCompleto, int edad, double peso, String email, boolean membresiaActiva, int sesionesRestantes) {
     this.nombreCompleto = nombreCompleto;
     this.edad = edad;
+    this.email = email;
     this.peso = peso;
     this.membresiaActiva = membresiaActiva;
     this.sesionesRestantes = sesionesRestantes;
   }
 
-  // Método void que imprime información del miembro
   public void imprimirInfo() {
-    System.out.println("~~~~ Información del miembro ~~~~");
+    System.out.println("~~~~~~ Información del miembro ~~~~~~");
     System.out.println("Nombre: " + nombreCompleto);
     System.out.println("Edad: " + edad + " años");
+    System.out.println("Correo electrónico: " + email);
     System.out.println("Peso: " + peso + " kg");
     System.out.println("Membresía activa: " + (membresiaActiva ? "Sí" : "No"));
     System.out.println("Sesiones restantes: " + sesionesRestantes);
   }
 
-  // Método que retorna un valor calculado (IMC en este caso)
   public double calcularIMC(double estaturaMetros) {
     return peso / (estaturaMetros * estaturaMetros);
   }
 
-  // Getter (ejemplo: obtener nombre)
-  public String getNombreCompleto() {
+    public String getNombreCompleto() {
     return nombreCompleto;
   }
 
-  // Setter (ejemplo: modificar peso)
   public void setPeso(double peso) {
     this.peso = peso;
   }
